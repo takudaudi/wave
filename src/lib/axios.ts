e21c5b896c0d05a43462d8db910dd409d63c518c
Taku daudi <takudaudi@gmail.com>
2025-10-23 14:30:08 +0200

    chenges

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
 	baseURL: import.meta.env.VITE_BACKEND_URL 
 		? `${import.meta.env.VITE_BACKEND_URL}/api` 
 		: import.meta.env.MODE === "development" 
-			? "http://localhost:5000/api" 
+			? "https://wavebackend.vercel.app/api" 
 			: "https://wavebackend.vercel.app/api",
 });
